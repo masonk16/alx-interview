54a356490f9ad60ddaec57f99f24c8ffbc82426a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-31 12:18:56 -0800

    add documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,13 +8,16 @@ def canUnlockAll(boxes):
     param boxes: a list of lists containing integers.
     return: True if all boxes can be opened, else return False.
     """
+
     unlocked_boxes = [0]
+    
     for box_index, box in enumerate(boxes):
         if not box:
             continue
         for key in box:
             if key < len(boxes) and key not in unlocked_boxes and key != box_index:
                 unlocked_boxes.append(key)
+    
     if len(unlocked_boxes) == len(boxes):
         return True
     return False
