3c3b18e3fde5ab32007e8909489ca36e971fe216
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-02 13:31:39 +0000

    pep8 validation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lockboxes Coding Challenge"""
+"""
+Lockboxes Coding Challenge
+"""
 
 
 def canUnlockAll(boxes):
@@ -9,15 +11,15 @@ def canUnlockAll(boxes):
     return: True if all boxes can be opened, else return False.
     """
 
-    unlocked_boxes = [0]
-    
-    for box_index, box in enumerate(boxes):
+    opened_boxes = [0]
+
+    for box_id, box in enumerate(boxes):
         if not box:
             continue
         for key in box:
-            if key < len(boxes) and key not in unlocked_boxes and key != box_index:
-                unlocked_boxes.append(key)
-    
-    if len(unlocked_boxes) == len(boxes):
+            if key < len(boxes) and key not in opened_boxes and key != box_id:
+                opened_boxes.append(key)
+
+    if len(opened_boxes) == len(boxes):
         return True
     return False
